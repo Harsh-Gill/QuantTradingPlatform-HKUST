6f2bebe9b19988e1eb3f482ec82de7bda02d52b2
harsh <harsh@mailtime.com>
2024-08-02 20:49:17 +0800

    updated exchange simulator with extended features on common object

--- Prototype/common/SingleStockExecution.py
+++ Prototype/common/SingleStockExecution.py
@@ -5,18 +5,35 @@ Created on Sat Jul 3 07:11:28 2019
 @author: hongs
 """
 
+import pandas as pd
+import random
+
 class SingleStockExecution():
-    
-    def __init__(self, ticker, date, timeStamp):
-        self.execID = 0
-        self.orderID = 0
+
+    def __init__(self,
+                 ticker,
+                 date,
+                 timeStamp,
+                 direction,
+                 execID,
+                 orderID,
+                 size=0,  # executed size
+                 price=None,
+                 order_status=None,
+                 rest_size=0):
+        self.execID = execID
+        self.orderID = orderID
         self.ticker = ticker
         self.date = date
         self.timeStamp = timeStamp
-        self.direction = None
-        self.price = None
-        self.size = None
-        self.comm = 0 #commission for this transaction
+        self.direction = direction  # "BI", "SI", "ACK", "Cancel Successfully But Partially Filled"
+        # "Cancel Failed fully filled", "Cancel Successfully not filled"
+        self.price = price
+        self.size = size
+        self.comm = 0  # commission for this transaction
+
+        self.order_status = order_status  # "Filled" "Partially Filled" "not filled"
+        self.rest_size = rest_size
 
     def outputAsArray(self):
         output = []
@@ -31,3 +48,6 @@ class SingleStockExecution():
         output.append(self.comm)
 
         return output
+
+
+
